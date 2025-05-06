1976af32ee3000dcd3ca7cde3e00537944d62c5b
yoki <yokithai17@gmail.com>
2025-05-06 23:37:31 +0300

    test

---
+++ sad/CMakeFiles/turing.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yoki/yadro/turing/main.cpp" "CMakeFiles/turing.dir/main.cpp.o" "gcc" "CMakeFiles/turing.dir/main.cpp.o.d"
+  "/home/yoki/yadro/turing/src/FileTape.cpp" "CMakeFiles/turing.dir/src/FileTape.cpp.o" "gcc" "CMakeFiles/turing.dir/src/FileTape.cpp.o.d"
+  "/home/yoki/yadro/turing/src/TapeSorter.cpp" "CMakeFiles/turing.dir/src/TapeSorter.cpp.o" "gcc" "CMakeFiles/turing.dir/src/TapeSorter.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
